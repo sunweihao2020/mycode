b6a54aac8b00081e2f0d8318cfbc54340aba0958
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-07-06 21:37:26 +0800

    add

--- paint/paint_meridional_pre_section_zonal_wind_sst.py
+++ paint/paint_meridional_pre_section_zonal_wind_sst.py
@@ -20,7 +20,7 @@ from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 path0  =  "/home/sun/qomo-data/"
 
-file1  =  xr.open_dataset('/home/sun/data/composite3.nc').sel(lon=slice(90,100),level=[925,850,700])
+file1  =  xr.open_dataset('/home/sun/data/composite3.nc').sel(lon=slice(90,100),level=slice(1000,100))
 file2  =  xr.open_dataset(path0+"composite_OISST.nc").sel(lon=slice(90,100))
 
 avg_u   =  np.nanmean(file1.uwind,axis=3)
@@ -47,11 +47,11 @@ newcmp.set_over('brown')
 def meridional_sst():
     '''This function paint meridional sst from -10 to 40. The purpose is to see the maximum sea temperature'''
     # set figure
-    fig1  =  plt.figure(figsize=(30,26))
+    fig1  =  plt.figure(figsize=(34,26))
     spec1 =  fig1.add_gridspec(nrows=3,ncols=3)
 
-    j = 0 ; #start = 30
-    start = 21
+    j = 0 ; start = 30
+    #start = 21
     for col in range(3):
         for row in range(3):
             ax  =  fig1.add_subplot(spec1[row,col])
@@ -64,7 +64,7 @@ def meridional_sst():
             ax.tick_params(axis='both',labelsize=22.5)
 
             # plot sst
-            ax.plot(file2.lat.data,avg_sst[start],linewidth=2.5,color='red',label="SST")
+            ax.plot(file2.lat.data,avg_sst[start],linewidth=3,color='red',label="SST")
 
             # set range
             ax.set_xlim((-10,30))
@@ -77,18 +77,24 @@ def meridional_sst():
             # add date
             add_text(ax=ax, string="D" + str(start-30), location=(0.05, 0.91), fontsize=30)
 
+            # add legend
+            plt.legend(loc='lower right', prop={'size': 20})
             # plot air temperature at 925 850 700hPa
             # second axis
             ax2  =  ax.twinx()
 
-            ax2.plot(file1.lat.data,avg_u[start,0],linewidth=2.5,label="925hpa")
+            ax2.plot(file1.lat.data,avg_u[start,0],linewidth=3,label="925hpa")
             ax2.plot(file1.lat.data, avg_u[start, 1],linewidth=2.5,label="850hpa")
-            ax2.plot(file1.lat.data, avg_u[start, 2],linewidth=2.5,label="700hpa")
+            ax2.plot(file1.lat.data, avg_u[start, 2],linewidth=3,label="700hpa")
+
+            ax2.tick_params(axis='both', labelsize=22.5)
+
+            plt.legend(loc='upper right',prop={'size': 20})
 
             start += 1
 
 
-    plt.savefig('/home/sun/paint/meridional_tem_gradient_circulation/meridional_sst_-9to-1.pdf', dpi=400)
+    plt.savefig('/home/sun/paint/meridional_tem_gradient_circulation/meridional_sst_0to+8.pdf', dpi=400)
     plt.show()
     #plt.plot(file2.lat.data,avg_sst[15])
     #plt.xlim([-10,30])
@@ -100,13 +106,13 @@ def meridional_section_uwind():
     fig1 = plt.figure(figsize=(30, 26))
     spec1 = fig1.add_gridspec(nrows=3, ncols=3)
     j = 0;
-    start = 30
+    start = 21
     for col in range(3):
         for row in range(3):
             ax = fig1.add_subplot(spec1[row, col])
             # set axis ticks and label
             ax.set_xticks(np.linspace(-10, 30, 9, dtype=int))
-            ax.set_yticks(np.linspace(1000, 200, 9))
+            ax.set_yticks(np.linspace(1000, 100, 10))
             ax.set_xticklabels(generate_xlabel(np.linspace(-10, 30, 9, dtype=int)))
             ax.tick_params(axis='both', labelsize=22.5)
             # plot sst
@@ -131,12 +137,12 @@ def meridional_section_uwind():
     cb = fig1.colorbar(im, cax=cbar_ax, shrink=0.1, pad=0.01, orientation='horizontal')
     cb.ax.tick_params(labelsize=25)
 
-    plt.savefig('/home/sun/paint/meridional_tem_gradient_circulation/meridional_uwind_90to100_0to+8.pdf', dpi=400)
+    plt.savefig('/home/sun/paint/meridional_tem_gradient_circulation/meridional_uwind_90to100_-9to-1.pdf', dpi=400)
     plt.show()
 
 def main():
-    #meridional_section_uwind()
-    meridional_sst()
+    meridional_section_uwind()
+    #meridional_sst()
 
 if __name__ == "__main__":
     main()
---
+++ paint/paint_meridional_streamfunction_composite.py
@@ -0,0 +1,4 @@
+'''
+2022-7-6
+This code paint regional streamfunction between 90 and 100E. Based on composite ncfile
+'''
