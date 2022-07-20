6642cd717741a746e1d512e6228aa82d742bf2ee
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-07-20 03:58:59 +0000

    change

--- paint/paint_monthly_temperature_90to100_avg.py
+++ paint/paint_monthly_temperature_90to100_avg.py
@@ -2,3 +2,21 @@
 2022-7-20
 This code is to calculate and paint monthly meri-vertical temperature
 '''
+import xarray as xr
+import numpy as np
+import os
+import matplotlib
+
+src_path  =  "/home/sun/data/merra2_climate_vars_multi/"
+
+def cal_monthly_tem():
+    import os
+    import xarray as xr
+    import numpy as np
+    '''This function calculate temperature between 90 to 100E
+       time period: Jan to May
+    '''
+    file_list  =  os.listdir(src_path) ; file_list.sort()
+
+def main():
+    cal_m
