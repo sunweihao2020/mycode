b5f6b5e2a0e7a29f9df0ecf0095fe939d5f805b1
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-07-20 14:02:12 +0000

    change

--- paint/paint_monthly_temperature_90to100_avg.py
+++ paint/paint_monthly_temperature_90to100_avg.py
@@ -6,6 +6,7 @@ import xarray as xr
 import numpy as np
 import os
 import matplotlib
+matplotlib.use('Agg')
 
 src_path  =  "/home/sun/data/merra2_climate_vars_multi/"
 
@@ -22,16 +23,17 @@ def cal_monthly_tem():
     '''
     file_list  =  os.listdir(src_path) ; file_list.sort()
     
-    date       =  [0,30,60,90,120,150]
+    date       =  [0,30,60,90,120,150,180]
+
 
     # reference file
     f0         =  xr.open_dataset(src_path+"0830.climate.nc").sel(lev=lev_slice,lon=lon_slice)
     
     # array to save
-    avg_tem    =  np.zeros((5,25,361))
+    avg_tem    =  np.zeros((6,25,361))
 
     # calculate monthly average
-    for i in range(5):
+    for i in range(6):
         # Jan to May
         start_day  =  date[i]
         end_day    =  date[i+1]
@@ -39,7 +41,7 @@ def cal_monthly_tem():
             f1        =  xr.open_dataset(src_path+file_list[dd]).sel(lev=lev_slice,lon=lon_slice)
 
             # calculate daily average
-            avg_dd_t  =  np.average(f1.T.data[0],axis=2)
+            avg_dd_t  =  np.nanmean(f1.T.data[0],axis=2)
 
             # save to array
             avg_tem[i]  +=  avg_dd_t/30  
@@ -56,13 +58,75 @@ def paint_meri_tem(tem):
     import sys
     sys.path.append("/home/sun/mycode/module/paint")
     import paint_lunwen_version3_0_fig1_bob_onset_seris as plv3
+    from paint_lunwen_version3_0_fig2a_tem_gradient_20220426 import create_label_lat
 
-    fig1    =  plt.figure(figsize=(26,17))
-    spec1   =  fig1.add_gridspec(nrows=2,ncols=2)
+    # month name
+    month     =  ['Jan','Feb','Mar','Apr','May','Jun']
 
+    # reference file
+    f0         =  xr.open_dataset(src_path+"0830.climate.nc").sel(lev=lev_slice,lon=lon_slice)
+
+    for pic in range(6):
+        fig, axs  =  plt.subplots(figsize=(13,13))
+
+        # contour lines
+        im  =  axs.contour(f0.lat,f0.lev,tem[pic],levels=set_levels(),colors='k',linewidths=2.34)
+        axs.clabel(im,inline_spacing=0, fontsize=15)
+
+        # deal with topography
+        topofile   =   xr.open_dataset("/home/sun/data/topography/bathymetric.nc").sel(lon=lon_slice)
+        dixing  =  topofile.elevation.data
+        dixing[dixing <= 0]  =  0
+        topo    =  np.average(dixing,axis=1)
+
+
+        # set label
+        axs.set_xticks(np.linspace(-10,40,6))
+        axs.set_xticklabels(create_label_lat(np.linspace(-10,40,6,dtype=int)))
+        axs.set_yticks(np.linspace(1000,200,5,dtype=int))
+        axs.set_yticklabels(np.linspace(1000,200,5,dtype=int))
+        axs.tick_params(axis='both',labelsize=20.5)
+
+        # set axis range
+        axs.set_xlim((-10,40))
+
+        axs.invert_yaxis()
+
+        # set title
+        axs.set_title(month[pic],fontsize=30)
+
+        # add topography graph
+        ax2  =  axs.twinx()
+
+        ax2.set_ylim((0,11.5))
+        ax2.tick_params(axis='both',labelsize=20.5)
+
+        ax2.plot(topofile.lat.data,topo/1000,color='k')
+        ax2.fill_between(topofile.lat.data,0,topo/1000,where=topo>0,color='k')
+
+
+        path_out = "/home/sun/paint/monthly_meri_vertical_tem_90to100E/" ; plv3.check_path(path_out)
+        file_out = "month"+str((pic+1))+"_tem.pdf"
+        plt.savefig(path_out+file_out,dpi=450)
+
+
+
+    
+def set_levels():
+    '''This function generate levels for contour, values above 290K space is 2K'''
+    array1  =  np.linspace(220,290,15)
+    array2  =  np.linspace(292,310,10)
+
+    level   =  np.hstack((array1,array2))
+
+    return level
 
 def main():
     avg_tem  =  cal_monthly_tem()
+    
+    paint_meri_tem(avg_tem)
+
+    
     
 
 if __name__ == "__main__":
