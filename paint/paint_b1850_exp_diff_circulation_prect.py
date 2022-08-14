0e8f4e9e1d47712f89e8a48f118af471c3ae7055
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-08-14 13:14:43 +0000

    change

---
+++ paint/paint_b1850_exp_diff_circulation_prect.py
@@ -0,0 +1,117 @@
+'''
+2022-7-23
+This script paint b1850 control experiment circulation and precipitation
+The daily resolution is pentad.Thus, in this script I need to calculate pentad average
+'''
+import os
+import sys
+import xarray as xr
+import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+from metpy.units import units
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+
+    
+def paint_pentad_circulation():
+    '''This function paint pentad circulation based on b1850 experiment'''
+    #  ----- import  ----------
+    from matplotlib import cm
+    from matplotlib.colors import ListedColormap
+
+    import sys
+    sys.path.append("/home/sun/mycode/paint/")
+    from paint_lunwen_version3_0_fig2b_2m_tem_wind_20220426 import set_cartopy_tick,save_fig,add_vector_legend
+    from paint_lunwen_version3_0_fig2a_tem_gradient_20220426 import add_text
+
+
+
+    import matplotlib
+    matplotlib.use('Agg')
+    ## ------------------------
+
+    path_out  =  "/home/sun/data/model_data/climate/"
+    file_out1  =  "b1850_control_atmospheric_monthly_average.nc"
+    file_out2  =  "b1850_maritime_atmospheric_monthly_average.nc"
+    f0        =  xr.open_dataset(path_out+file_out1)
+    f1        =  xr.open_dataset(path_out+file_out2)
+    ## -------------------------------------------------
+
+
+    # ------  time period  -----------
+    pentads   =  [5,6,7,8]
+    date      =  [6,7,8,9]
+
+    # -------  color bar   -----------
+    viridis = cm.get_cmap('coolwarm', 16)
+    newcolors = viridis(np.linspace(0, 1, 16))
+    newcmp = ListedColormap(newcolors)
+    #newcmp.set_under('white')
+    #newcmp.set_over('#004080')
+
+    # -------   cartopy extent  -----
+    lonmin,lonmax,latmin,latmax  =  30,150,-10,70
+    extent     =  [lonmin,lonmax,latmin,latmax]
+
+    # -------     figure    -----------
+    proj  =  ccrs.PlateCarree()
+    fig1    =  plt.figure(figsize=(32,24))
+    spec1   =  fig1.add_gridspec(nrows=2,ncols=2)
+
+    j  =  0
+
+    # ------       paint    ------------
+    for col in range(2):
+        for row in range(2):
+            ax = fig1.add_subplot(spec1[row,col],projection=proj)
+
+            # 设置刻度
+            set_cartopy_tick(ax=ax,extent=extent,xticks=np.linspace(30,150,7,dtype=int),yticks=np.linspace(-10,70,9,dtype=int),nx=1,ny=1,labelsize=25)
+
+            # 添加赤道线
+            ax.plot([40,120],[0,0],'k--')
+
+            im  =  ax.contourf(f0.lon,f0.lat,(f0["PRECT"].data[pentads[j]]*86400000 - f1["PRECT"].data[pentads[j]]*86400000)/(f0["PRECT"].data[pentads[j]]*86400000)*100,np.linspace(-100,100,21),cmap=newcmp,alpha=1,extend='both')
+
+            # 海岸线
+            ax.coastlines(resolution='50m',lw=2)
+
+            print((f0["U"].data[pentads[j]]-f1["U"].data[pentads[j]]).shape)
+            q  =  ax.quiver(f0.lon.data, f0.lat.data, (f0["U"].data[pentads[j],3]-f1["U"].data[pentads[j],3]), (f0["V"].data[pentads[j],3]-f1["V"].data[pentads[j],3]), 
+                regrid_shape=20, angles='uv',   # regrid_shape这个参数越小，是两门就越稀疏
+                scale_units='xy', scale=0.3,        # scale是参考矢量，所以取得越大画出来的箭头就越短
+                units='xy', width=0.35,
+                transform=proj,
+                color='k',linewidth=1.2,headlength = 5, headaxislength = 4, headwidth = 5,alpha=1)
+
+            # 加日期
+            add_text(ax=ax,string="month"+str(date[j]),location=(0.80,0.91),fontsize=25)
+
+            # 加矢量图图例
+            add_vector_legend(ax=ax,q=q)
+        
+            j += 1
+    
+    # 加colorbar
+    fig1.subplots_adjust(top=0.8) 
+    cbar_ax = fig1.add_axes([0.2, 0.05, 0.6, 0.03]) 
+    cb  =  fig1.colorbar(im, cax=cbar_ax, shrink=0.1, pad=0.01, orientation='horizontal')
+    cb.ax.tick_params(labelsize=20)
+
+    save_fig(path_out="/home/sun/paint/b1850_exp/",file_out="b1850_diff_monthly_925_circulation_with_prect.pdf")
+
+
+
+def main():
+    paint_pentad_circulation()
+    
+
+
+
+
+
+if __name__  ==  "__main__":
+    main()
---
+++ paint/paint_b1850_maritime_hov_prect.py
@@ -0,0 +1,100 @@
+'''
+2022-8-10
+This script paint hov for experiment: b1850 global1m
+'''
+import sys
+import xarray as xr
+import numpy as np
+module_path = ["/home/sun/mycode/module/","/data5/2019swh/mycode/module/"]
+sys.path.append(module_path[0])
+from module_sun import *
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.gridspec as gridspec
+import matplotlib
+matplotlib.use('Agg')
+
+sys.path.append("/home/sun/mycode/paint/")
+from paint_lunwen_version3_0_fig2b_2m_tem_wind_20220426 import save_fig
+
+
+'''设置colormap、levels、坐标值'''
+# 坐标轴设置
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+viridis = cm.get_cmap('Blues', 16)
+newcolors = viridis(np.linspace(0, 1, 16))
+newcmp = ListedColormap(newcolors)
+newcmp.set_under('white')
+newcmp.set_over('#19345e')
+#levels
+clevs = np.arange(3,20,1)
+# Tick labels
+x_tick_labels = []
+for xx in range(70,131,20):
+    x_tick_labels.append(u''+str(xx)+"\N{DEGREE SIGN}E")
+y_tick = [100,110,120,130,140,150,161,171,181,191,201]
+y_label = ['10Apr','20Apr','30Apr','10May','20May','30May','10Jun','20Jun','30Jun','10Jul','20Jul']
+
+
+
+def lon_avg_prect(time_slice=slice(100,201),lat_slice=slice(10,20),lon_slice=slice(65,130)):
+    '''This function deal with precipitation data'''
+    # model result
+    control_prect   =  xr.open_dataset("/home/sun/data/model_data/climate/b1850_maritime_atmosphere.nc").sel(time=time_slice,lev=925,lat = lat_slice,lon = lon_slice)
+
+    # calculate weight average
+    avg_control_prect  =  (control_prect["PRECT"]*(np.cos(np.deg2rad(control_prect.lat.values)))[None, :, None]).sum(dim='lat')/np.sum(np.cos(np.deg2rad(control_prect.lat.values)))*86400*1000
+
+    #avg_control_prect  =  (control_prect["U"]*(np.cos(np.deg2rad(control_prect.lat.values)))[None, :, None]).sum(dim='lat')/np.sum(np.cos(np.deg2rad(control_prect.lat.values)))
+    return avg_control_prect
+
+def paint_trmm_hov(control,path_out,filename,time_slice=slice(100,201),lat_slice=slice(10,20),lon_slice=slice(65,130)):
+    '''This function paint control prect with map'''
+    f0  =  xr.open_dataset("/home/sun/data/model_data/climate/b1850_maritime_atmosphere.nc").sel(time=time_slice,lat = lat_slice,lon = lon_slice)
+    # Start figure
+    fig = plt.figure(figsize=(13, 15))
+    gs = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[2, 6], hspace=0.03)
+
+
+    # Top plot for geographic reference (makes small map)
+    ax1 = fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree())
+    ax1.set_extent([65, 130, 10, 20], ccrs.PlateCarree())
+    # 设置y轴刻度
+    ax1.set_yticks([10, 20])
+    ax1.set_yticklabels([u'10\N{DEGREE SIGN}N', u'20\N{DEGREE SIGN}N'],fontsize=25)
+    # 设置x轴刻度
+    ax1.set_xticks(np.arange(70,131,20))
+    ax1.set_xticklabels(x_tick_labels,fontsize=25)
+    ax1.grid(linestyle='dotted', linewidth=2)
+
+    ax1.add_feature(cfeature.COASTLINE.with_scale('10m'))
+    ax1.add_feature(cfeature.LAKES.with_scale('50m'), color='black', linewidths=0.05)
+    ax1.add_feature(cfeature.RIVERS)
+    ax1.add_feature(cfeature.LAKES)
+    ax1.stock_img()
+
+    ax2 = fig.add_subplot(gs[1, 0])
+
+    cf   = ax2.contourf(f0.lon.values, f0.time.values, control.data,clevs, cmap=newcmp, extend='both')
+    cs   = ax2.contour(f0.lon.values,  f0.time.values, control.data,clevs, colors='k', linewidths=1)
+
+
+    # 坐标设置
+    ax2.set_xticks(np.arange(70,131,20))
+    ax2.set_xticklabels(x_tick_labels,fontsize=25)
+
+    ax2.set_yticks(y_tick)
+    ax2.set_yticklabels(y_label,fontsize=25)
+
+    save_fig(path_out=path_out,file_out=filename)
+
+
+def main():
+    control = lon_avg_prect()
+    paint_trmm_hov(control,path_out="/home/sun/paint/b1850_exp/",filename="maritime_hov_prect.pdf")
+
+
+if __name__ == "__main__":
+    main()
