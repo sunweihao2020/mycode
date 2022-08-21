67fbeea63bd71797f0c1a9e8153962887f1dee03
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-08-21 08:48:07 +0000

    add new

---
+++ paint/paint_jan_jul_vert_meri_circulation_bob_region.py
@@ -0,0 +1,30 @@
+'''
+This code paint January and July meridional circulation in the BOB region
+To depict the characteristic of the Hadley cell
+'''
+path0  =  "/home/sun/data/merra2_climate_vars_multi/daily/"
+path1  =  "/home/sun/data/merra2_climate_vars_multi/monthly/"
+
+def cal_jan_jul_average():
+    '''This function calculate climate monthly vars average and save to the path'''
+    import numpy as np
+    import xarray as xr
+    import os
+
+    # get the files list
+    file_list  =  os.listdir(path0) ; file_list.sort()
+
+    # month days
+    days       =  [31,28,31,30,31,30,31,31,30,31,30,31]
+
+    # calculate monthly
+    ## reference file
+    f0         =  xr.open_dataset(path0+"01101")
+
+    print(np.sum(days))
+
+def main():
+    cal_jan_jul_average()
+
+if __name__ == "__main__":
+    main()
