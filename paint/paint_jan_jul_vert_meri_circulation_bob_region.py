8d60366e6319aa0fad5c3d7eb4328d1ebdf1b234
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-08-21 21:22:26 +0800

    new change

--- paint/paint_jan_jul_vert_meri_circulation_bob_region.py
+++ paint/paint_jan_jul_vert_meri_circulation_bob_region.py
@@ -27,20 +27,58 @@ def cal_jan_jul_average():
     ### get datasets vars name
     vars = list(f0.keys())
 
-    ## calculate monthly average
+    ## --------calculate monthly average----------
+    ### create  empty dataset
+    monthly_avg  =  xr.Dataset()
     for vvvv in vars:
         base  =  create_base_array(f0[vvvv])
 
         for mon in range(len(days)):
-            start   = np.sum(days[0:mon])
-            end     = np.sum(days[0:(mon+1)])
+            start   = int(np.sum(days[0:mon]))
+            end     = int(np.sum(days[0:(mon+1)]))
             period  = end-start
             #print("start is %d" % start)
             #print("end is %d" % end)
+
             for dd in range(start,end):
-                f1    = xr.open_dataset(path0+file_list[dd])
+                print(type(dd))
+                f1         =   xr.open_dataset(path0+file_list[dd])
                 base[mon]  +=  f1[vvvv].data[0] / period
 
+        # save array to xarray dataarray
+        monthly_avg[vvvv]  =  create_base_dataarray(base)
+        print(monthly_avg)
+
+def create_base_dataarray(array):
+    '''This function create dataarray for input array'''
+    import xarray as xr
+    import numpy as np
+
+    # reference file
+    f0   =   xr.open_dataset(path0 + "0101.climate.nc")
+    dim  =   len(array.shape)
+    if dim == 3:
+        da = xr.DataArray(
+            data=array,
+            dims=["time","lat","lon"],
+            coords={
+                "time":np.linspace(1,12,12),
+                "lat":f0.lat.data,
+                "lon":f0.lon.data,
+            },
+        )
+    else:
+        da = xr.DataArray(
+            data=array,
+            dims=["time","lev", "lat", "lon"],
+            coords={
+                "time": np.linspace(1, 12, 12),
+                "lat": f0.lat.data,
+                "lon": f0.lon.data,
+                "lev": f0.lev.data,
+            },
+        )
+        return da
 
 def create_base_array(array):
     '''This function returns base array based on the input array'''
@@ -59,9 +97,19 @@ def create_base_array(array):
     return base_array
 
 
+def test():
+    import xarray as xr
 
+    # reference file
+    f0 = xr.open_dataset(path0 + "0101.climate.nc")
+    a  = list(f0.attrs)
+    print(f0["H"].attrs.keys())
 def main():
+    import warnings
+    warnings.filterwarnings("ignore")
+
     cal_jan_jul_average()
+
 
 if __name__ == "__main__":
     main()
