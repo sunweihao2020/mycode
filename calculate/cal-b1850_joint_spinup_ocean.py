3c16b696a1805e3c58c7fb5bc61b0969be5df6e9
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-09-11 02:09:13 +0000

    new script deal with b1850

---
+++ calculate/cal-b1850_joint_spinup_ocean.py
@@ -0,0 +1,24 @@
+'''
+2022-9-10
+This code joint the series for the spin-up ocean model output
+'''
+
+'''
+Here is the summary for the spinup experiment:
+1. control:     1.1  b1850m_control3_220617 01 - 52
+                1.2  b1850_control4_220624  53 - 96
+                This may be branch run in these experiments
+
+2. indian:      2.1  b1850_tx_indian_h0_220718 01 - 58
+                2.2  b1850_tx_indian_h1_220726 59 - 84
+                2.3  b1850_tx_indian_h2_220731 01 - 26
+
+3. maritime:    3.1  b1850_tx4_maritime_220624     01 - 26
+                3.2  b1850_tx_maritime_h1_220629   01 - 46
+                3.3  b1850_tx_maritime_h1_220721   47 - 58
+                3.4  b1850_tx_maritime_h2_220725   59 - 94
+
+4. inch:        4.1  b1850_tx_inch_220811  01 - 90
+
+5. global1m     5.1  b1850_global_1m_220620 21 - 98
+'''
