0d510fab916ea73d3b3e8a9a44ac2e02ebef8074
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2022-10-17 14:46:33 +0800

    accomplish data process

--- paint/paint_compare_current_b1850_221012.py
+++ paint/paint_compare_current_b1850_221012.py
@@ -55,10 +55,177 @@ def paint_jja_850_precip_wind(extent,lon_slice,lat_slice):
     f2  =  xr.open_dataset(b1850_control)
 
     ## 1. The resolution between MERRA-2 and CESM2 is different, I need intepolate it to keep them consistent
-    merra_les  =  interp_merra_to_cam(f1)
+    merra_les_u,merra_les_v  =  interp_merra_to_cam(f1,f2)
+
+    '''calculate wind speed for merra2 and model'''
+    speed_merra = np.sqrt(np.square(merra_les_u) + np.square(merra_les_v))
+    speed_model = np.sqrt(np.square(f2.U.data)   + np.square(f2.V.data))
+
     
-def interp_merra_to_cam(f):
-    print(f)
+def interp_merra_to_cam(f,ref_f):
+    '''This function intepolate merra2 variables to the B1850 output. Variables include U V'''
+    import numpy as np
+    import time
+
+    '''Array save the result'''
+    merra_u  =  ref_f.U.data.copy()
+    merra_v  =  merra_u.copy()
+
+    '''information of the lat/lon'''
+    # print(f.lon.data)      # -180 - 179.375 ; position 288 corresponds to 0
+    # print(ref_f.lon.data)  # 0-368.75
+
+    ''' transfer merra2 origin data's longitude
+        [288 to end] + [0 to 288] sequence
+    '''
+    origin_merra_u  =  f.U.data.copy()
+    origin_merra_v  =  origin_merra_u.copy()
+
+    origin_merra_u[:,:,:,0:288]  =  f.U.data[:,:,:,288:]
+    origin_merra_u[:,:,:,288:]   =  f.U.data[:,:,:,0:288]
+    origin_merra_v[:, :, :, 0:288] = f.V.data[:, :, :, 288:]
+    origin_merra_v[:, :, :, 288:]  = f.V.data[:, :, :, 0:288]
+
+    # change the data in the f
+    f.U.data  =  origin_merra_u
+    f.V.data  =  origin_merra_v
+
+    # replace the old longitude for merra-2
+    new_lon          =   np.zeros((576))
+    new_lon[0:288]   =   f.lon.data[288:]
+    new_lon[288:]    =   f.lon.data[0:288]   +  360
+
+    #print(new_lon)  # 0 - 359.375
+
+    '''interpolate origin data to the new longitude
+       lev lat lon need to be interpolated 
+       Thus three interim array are needed
+       Here I declare a function for this process
+    '''
+    start  =  time.time()
+    interp_u  =  interpolate_4d_resolution_second(f,ref_f,'U')
+    interp_v  =  interpolate_4d_resolution_second(f,ref_f,'V')
+    print('interpolation spend %f second' % (time.time() - start))
+
+    return interp_u,interp_v
+
+
+
+
+def interpolate_4d_resolution(f,ref_f,varname):
+    '''This function can interpolate f to the ref_f's resolution
+       dimensions should be (time,lev,lat,lon)
+       The time dimension have to be the same
+    '''
+    import numpy as np
+
+    '''I need to declare 3 interim array for interpolation
+       1. interpolate from right to left
+       2. interpolate from left to right
+    '''
+    interp_lev  =  np.zeros((f[varname].shape[0],len(ref_f['lev']),f[varname].shape[2],f[varname].shape[3]))
+    interp_lat  =  np.zeros((f[varname].shape[0],len(ref_f['lev']),len(ref_f['lat']),f[varname].shape[3]))
+    interp_lon  =  np.zeros(ref_f[varname].shape)
+
+    for xx in range(len(f['lon'])):
+        for yy in range(len(f['lat'])):
+            for tt in range(len(f['time'])):
+                interp_lev[tt,:,yy,xx]  =  np.interp(ref_f['lev'].data,f['lev'].data,f[varname].data[tt,:,yy,xx])
+
+    for xx in range(len(f['lon'])):
+        for ll in range(len(ref_f['lev'])):
+            for tt in range(len(f['time'])):
+                interp_lat[tt,ll,:,xx]  =  np.interp(ref_f['lat'].data,f['lat'].data,interp_lev[tt,ll,:,xx])
+
+    # replace the old longitude for merra-2
+    new_lon = np.zeros((576))
+    new_lon[0:288] = f.lon.data[288:]
+    new_lon[288:] = f.lon.data[0:288] + 360
+
+    for yy in range(len(ref_f['lat'])):
+        for ll in range(len(ref_f['lev'])):
+            for tt in range(len(ref_f['time'])):
+                interp_lon[tt,ll,yy,:]  =  np.interp(ref_f['lon'].data,new_lon,interp_lat[tt,ll,yy,:])
+
+    return interp_lon
+
+def interpolate_4d_resolution_second(f,ref_f,varname):
+    '''This function can interpolate f to the ref_f's resolution
+       dimensions should be (time,lev,lat,lon)
+       The time dimension have to be the same
+    '''
+    import numpy as np
+
+    '''I need to declare 3 interim array for interpolation
+       1. interpolate from right to left
+       2. interpolate from left to right
+    '''
+    interp_lon  =  np.zeros((f[varname].shape[0],f[varname].shape[1],f[varname].shape[2],len(ref_f['lon'])))
+    interp_lat  =  np.zeros((f[varname].shape[0],f[varname].shape[1],len(ref_f['lat']),len(ref_f['lon'])))
+    interp_lev  =  np.zeros(ref_f[varname].shape)
+
+    # replace the old longitude for merra-2
+    new_lon = np.zeros((576))
+    new_lon[0:288] = f.lon.data[288:]
+    new_lon[288:] = f.lon.data[0:288] + 360
+
+    for tt in range(len(f['time'])):
+        for ll in range(len(f['lev'])):
+            for yy in range(len(f['lat'])):
+                interp_lon[tt,ll,yy,:]  =  np.interp(ref_f['lon'].data,new_lon,f[varname].data[tt,ll,yy,:])
+
+    for tt in range(len(f['time'])):
+        for ll in range(len(f['lev'])):
+            for xx in range(len(ref_f['lon'])):
+                interp_lat[tt,ll,:,xx]  =  np.interp(ref_f['lat'].data,f['lat'].data,interp_lon[tt,ll,:,xx])
+
+
+    for tt in range(len(ref_f['time'])):
+        for yy in range(len(ref_f['lat'])):
+            for xx in range(len(ref_f['lon'])):
+                interp_lev[tt,:,yy,xx]  =  np.interp(ref_f['lev'].data,f['lev'].data,interp_lat[tt,:,yy,xx])
+
+    return interp_lev
+
+def interpolate_4d_resolution_third(f,ref_f,varname):
+    '''This function can interpolate f to the ref_f's resolution
+       dimensions should be (time,lev,lat,lon)
+       The time dimension have to be the same
+    '''
+    import numpy as np
+
+    '''I need to declare 3 interim array for interpolation
+       1. interpolate from right to left
+       2. interpolate from left to right
+    '''
+    interp_lev  =  np.zeros((f[varname].shape[0],len(ref_f['lev']),f[varname].shape[2],f[varname].shape[3]))
+    interp_lat  =  np.zeros((f[varname].shape[0],len(ref_f['lev']),len(ref_f['lat']),f[varname].shape[3]))
+    interp_lon  =  np.zeros(ref_f[varname].shape)
+
+    for tt in range(len(f['time'])):
+        for yy in range(len(f['lat'])):
+            for xx in range(len(f['lon'])):
+                interp_lev[tt,:,yy,xx]  =  np.interp(ref_f['lev'].data,f['lev'].data,f[varname].data[tt,:,yy,xx])
+
+    for tt in range(len(f['time'])):
+        for ll in range(len(ref_f['lev'])):
+            for xx in range(len(f['lon'])):
+                interp_lat[tt,ll,:,xx]  =  np.interp(ref_f['lat'].data,f['lat'].data,interp_lev[tt,ll,:,xx])
+
+    # replace the old longitude for merra-2
+    new_lon = np.zeros((576))
+    new_lon[0:288] = f.lon.data[288:]
+    new_lon[288:] = f.lon.data[0:288] + 360
+
+    for tt in range(len(ref_f['time'])):
+        for ll in range(len(ref_f['lev'])):
+            for yy in range(len(ref_f['lat'])):
+                interp_lon[tt,ll,yy,:]  =  np.interp(ref_f['lon'].data,new_lon,interp_lat[tt,ll,yy,:])
+
+    return interp_lon
+
+
+
 
 def main():
     lonmin,lonmax,latmin,latmax  =  30,120,-10,40
