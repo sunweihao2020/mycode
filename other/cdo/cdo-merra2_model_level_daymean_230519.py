3836eb674075ead3019140de5da1d21e09f97098
sunweihao2020 <sunweihao@lasg.iap.ac.cn>
2023-05-22 07:18:29 +0000

    new

--- other/cdo/cdo-merra2_model_level_daymean_230519.py
+++ other/cdo/cdo-merra2_model_level_daymean_230519.py
@@ -4,7 +4,8 @@ This script is to cdo daymean the downloaded merra2 modellevel files
 '''
 import os
 
-path0 = '/home/sun/mydown/merra_modellev/'
+path0 = '/home/sun/wd_disk/merra2_modellev/source/'
+path1 = '/home/sun/wd_disk/merra2_modellev/daymean/'
 
 file0 = os.listdir(path0)
 file1 = []
@@ -16,13 +17,15 @@ for ffff in file0:
 
             continue
 
-print(len(file1)/365)
-
-for yyyy in range(1980, 2020):
-    file3 = []
-    str1 = '.'+str(yyyy)
-    for ffff in file0:
-        if str1 in ffff:
-            file3.append(ffff)
-
-    print('This is year {}, file number is {}'.format(yyyy, len(file3)))
+#print(len(file1)/365)
+#
+#for yyyy in range(1980, 2020):
+#    file3 = []
+#    str1 = '.'+str(yyyy)
+#    for ffff in file0:
+#        if str1 in ffff:
+#            file3.append(ffff)
+#
+#    print('This is year {}, file number is {}'.format(yyyy, len(file3)))
+for ffff in file1:
+    os.system("cdo daymean "+path0+ffff+" "+path1+ffff)
